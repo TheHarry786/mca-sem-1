e62dffd293042e5291a9f151f0de3351588771c1
TheHarry786 <96932943+TheHarry786@users.noreply.github.com>
2022-02-01 17:09:17 +0530

    Delete fileupload directory

--- fileupload/index.php
+++
@@ -1,7 +0,0 @@
-<form action="upload.php" method="post" enctype="multipart/form-data">
-
-		Select image Upload :
-		<input type="file" name="fileToUpload" id="fileToUpload">
-		<input type="submit" name="Submit" value="Upload Image">
-
-</form>
