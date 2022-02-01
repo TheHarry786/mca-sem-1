f86d9a13dac8ed534ebc9ed2ee9a296c95737251
TheHarry786 <96932943+TheHarry786@users.noreply.github.com>
2022-02-01 16:35:55 +0530

    Delete 25 directory
    .

--- 25/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = mysqli_connect("localhost", "root", "", "test");
-
-if (mysqli_connect_errno()) {
-	printf("Connect failed: %s\n", mysqli_connect_error());
-	exit();
-}
-?><br/><br/>
